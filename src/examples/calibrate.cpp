6362ce84151b8570d4901807f6e9246d482cff51
0nhc <thefoxfoxfox@outlook.com>
2023-06-21 17:21:51 +0800

    add examples folder

---
+++ src/examples/calibrate.cpp
@@ -0,0 +1,22 @@
+#include <arx5_base/hardware_interface.h>
+
+int main()
+{
+  // 生成硬件接口对象
+  HardwareInterface hardware_interface("real");
+
+  while (1)
+  {
+    // 控制关节
+    hardware_interface.setJointAngles({ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 });
+    // 获取关节状态
+    std::vector<double> joint_states = hardware_interface.getJointStates();
+    // 打印关节状态
+    for (int i = 0; i < 6; i++)
+    {
+      std::cout << "joint_states[" << i << "] = " << joint_states[i] << std::endl;
+    }
+  }
+
+  return 0;
+}
